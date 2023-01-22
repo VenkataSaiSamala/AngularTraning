10950c22b94530178bd7a5c872aa0e7ffd918e0a
venkata sai kumar samala <venkat7sai@outlook.com>
2023-01-22 01:35:28 -0500

    Components-1

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent {
+ingredients : Ingredient[] = [
+  new Ingredient('Apples', 5),
+  new Ingredient('Pears', 4)
+];
+ 
+constructor(){
+
+}
+
+ngOnInit(){
+
+}
+
+}
